16b6ba400f765ca36c282f408dc4d5931181bf28
Michal Kadusi <michal2@mail.sapir.ac.il>
2025-11-22 19:15:26 +0200

    Game(PartTwo.java): Add PartTwo.java

---
+++ src/main/java/com/sapir/files/PartTwo.java
@@ -0,0 +1,60 @@
+package com.sapir.files;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class PartTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String studentsFile = scanner.nextLine();
+        String gradesFile = scanner.nextLine();
+
+        String outputFile = "report.txt";
+
+        try (BufferedReader brStudents = new BufferedReader(new FileReader(studentsFile));
+                BufferedReader brGrades = new BufferedReader(new FileReader(gradesFile));
+                PrintWriter pw = new PrintWriter(new FileWriter(outputFile))) {
+
+            String lineStudent = brStudents.readLine();
+            String lineGrade = brGrades.readLine();
+
+            while (lineStudent != null && lineGrade != null) {
+                String[] studentInfo = lineStudent.split(" ");
+                String idStudent = studentInfo[studentInfo.length - 1];
+                long id = Long.parseLong(idStudent);
+
+                String[] grade = lineGrade.split(" ");
+                long idGrade = Long.parseLong(grade[0]);
+
+                if (id < idGrade) {
+                    lineStudent = brStudents.readLine();
+                } else if (idGrade < id) {
+                    lineGrade = brGrades.readLine();
+                }
+
+                int grade1 = Integer.parseInt(grade[1]);
+                int grade2 = Integer.parseInt(grade[2]);
+                int avg = (grade1 + grade2) / 2;
+
+                StringBuilder fullName = new StringBuilder();
+                for (int i = 0; i < studentInfo.length - 1; i++) {
+                    fullName.append(studentInfo[i]).append(" ");
+                }
+                pw.println(id + " " + fullName.toString().trim() + " " + avg);
+
+                lineStudent = brStudents.readLine();
+                lineGrade = brGrades.readLine();
+
+            }
+        }
+
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            System.out.println("Error parsing data (check file format): " + e.getMessage());
+        }
+
+    }
+}
